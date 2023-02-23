2a70d4702b4527c254d5c6402724a03498b98547
Isabella <66403691+isabella-talijancic@users.noreply.github.com>
2023-02-23 14:22:51 -0600

    first commit

---
+++ Convex Hull/src/Project1.java
@@ -0,0 +1,16 @@
+/**
+ * Project1 is a Java class containing a main method to run the program when completed.
+ * 
+ * @author Isabella Taliancic (juu530)
+ * @author Amalia Taliancic (juu530)
+ * UTSA CS 3343 - Project 1
+ * Fall 2023
+ */
+public class Project1 {
+	
+	public static void main( String[] args ) {
+		
+		// Print to the console
+		System.out.println( "Test");
+	}
+}
